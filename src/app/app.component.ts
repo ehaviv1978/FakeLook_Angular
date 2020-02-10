635d191227b8ea0b3bac7cfdd35b0802a9099358
ehaviv1978 <ehaviv@hotmail.com>
2020-02-10 15:03:37 +0200

    First Angular commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fakelookApp';
+  users: any = [];
+  posts: any =[];
+  search: string;
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    this.http.get('http://localhost:8888/api/users').subscribe(res => {
+      this.users = res;
+      console.log(this.users)
+    });
+  }
+
+  searchUsers() {
+    this.http.get(`http://localhost:8888/api/users/${this.search}`).subscribe(res => {
+      this.users = res;
+      console.log(this.users)
+    });
+  }
+
+  getPosts() {
+    this.http.get('http://localhost:8888/api/Posts').subscribe(res => {
+      this.posts = res;
+      console.log(this.users)
+    });
+  }
+
+  searchBox(s:string) {
+    this.search = s;
+  }
+
 }
