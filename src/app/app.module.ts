ef281f2a997b5dfeab969ef02675c101911ff5ce
ehaviv1978 <ehaviv@hotmail.com>
2020-02-12 15:27:31 +0200

    sidebar add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { TitleComponent } from './title/title.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { MapPostsComponent } from './map-posts/map-posts.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { AllUsersComponent } from './all-users/all-users.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     MenuBarComponent,
     MapPostsComponent,
     NavBarComponent,
+    SideBarComponent,
+    AllUsersComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/map-posts/map-posts.component.ts
+++ src/app/map-posts/map-posts.component.ts
@@ -1,45 +1,52 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'map-posts',
   templateUrl: './map-posts.component.html',
   styleUrls: ['./map-posts.component.css']
 })
-export class MapPostsComponent  implements AfterViewInit {
+export class MapPostsComponent implements AfterViewInit {
   title = 'angular-gmap';
   @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
   map: google.maps.Map;
-  lat = 31.76519;
-  lng = 34.96168;
-  
-  if (navigator)
-    {
-      navigator.geolocation.getCurrentPosition( pos => {
-      this.lng = +pos.coords.longitude;
-      this.lat = +pos.coords.latitude;
-  });
-}
+  lat: any;
+  lng: any;
+  coordinates:google.maps.LatLng;
+mapOptions:google.maps.MapOptions;
+marker:google.maps.Marker;
 
-  coordinates = new google.maps.LatLng(this.lat, this.lng);
-
-  mapOptions: google.maps.MapOptions = {
-    center: this.coordinates,
-    zoom: 8
-  };
-
-  marker = new google.maps.Marker({
-    position: this.coordinates,
-    map: this.map,
-    title: 'Hello World!'
-  });
+  getCurrentLocation(callback) {
+    if (navigator) {
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.lng = pos.coords.longitude;
+        this.lat = pos.coords.latitude;
+        this.coordinates = new google.maps.LatLng(this.lat, this.lng);
+        this.mapOptions = {
+          center: this.coordinates,
+          zoom: 8
+        };
+        this.marker = new google.maps.Marker({
+          position: this.coordinates,
+          map: this.map,
+          title: 'Hello World!'
+        });
+        callback();
+      });
+    }
+  }
 
   ngAfterViewInit() {
-    this.mapInitializer();
+    this.getCurrentLocation(() => {
+      console.log(this.lat)
+      console.log(this.lng)
+      this.mapInitializer();
+    });
   }
 
   mapInitializer() {
     this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+    debugger
     this.marker.setMap(this.map);
   }
 }
