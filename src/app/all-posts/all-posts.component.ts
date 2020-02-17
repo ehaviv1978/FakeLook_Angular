7640b479cc6fd3deeb92121df5dfc2faff63e735
ehaviv1978 <ehaviv@hotmail.com>
2020-02-17 14:24:53 +0200

    get post picture

--- src/app/all-posts/all-posts.component.ts
+++ src/app/all-posts/all-posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { DomSanitizer,SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-all-posts',
@@ -8,18 +9,30 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AllPostsComponent implements OnInit {
   posts: any =[];
-  picUrl ;
-  constructor(private http: HttpClient) { }
+  post: any;
+  img:any;
+  imageurl:SafeUrl;
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
 
   getPosts() {
     this.http.get('http://localhost:8888/api/Posts').subscribe(res => {
       this.posts = res;
+      console.log(this.posts);
     });
   }
 
-  showPic(event){
-    this.picUrl=event.toElement.title;
-    console.log(event.toElement.title)
+  async showPic(event){
+    this.post = this.posts[event.toElement.id];
+    const TYPED_ARRAY = new Uint8Array(this.post.Image.data);
+    console.log(TYPED_ARRAY);
+
+    const STRING_CHAR = await String.fromCharCode.apply(null, TYPED_ARRAY);
+    await console.log(STRING_CHAR);
+
+    let base64String = await btoa(STRING_CHAR);
+    await console.log(base64String);
+    this.imageurl = this.sanitizer.bypassSecurityTrustUrl('data:image/jpeg;charset=utf-8;base64,' + base64String);
+  //  console.log(event.toElement.title)
   }
 
   ngOnInit() {
