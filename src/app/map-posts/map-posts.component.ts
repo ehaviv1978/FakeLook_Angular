520eadafa48a597779038e7ad195383e58413693
ehaviv1978 <ehaviv@hotmail.com>
2020-02-18 09:30:50 +0200

    posts location add to the map

--- src/app/map-posts/map-posts.component.ts
+++ src/app/map-posts/map-posts.component.ts
@@ -1,5 +1,7 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { DomSanitizer,SafeUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'map-posts',
@@ -13,8 +15,32 @@ export class MapPostsComponent implements AfterViewInit {
   lat: any;
   lng: any;
   coordinates:google.maps.LatLng;
-mapOptions:google.maps.MapOptions;
-marker:google.maps.Marker;
+  mapOptions:google.maps.MapOptions;
+  marker:google.maps.Marker;
+  posts: any =[];
+  post: any;
+  img:any;
+  imageurl:SafeUrl;
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
+
+  async showPosts() {
+    this.http.get('http://localhost:8888/api/Posts').subscribe(async res => {
+      this.posts = await res;
+      console.log(this.posts[0]);
+      for (var post of this.posts) {
+        this.lng = await post.Long;
+        console.log(this.lng);
+        this.lat = await post.Lat;
+        this.coordinates = await new google.maps.LatLng(this.lat, this.lng);
+        this.marker = await new google.maps.Marker({
+          position: this.coordinates,
+            map: this.map,
+            title: post.Description
+          });
+          await this.marker.setMap(this.map);
+      }
+    });
+  }
 
   getCurrentLocation(callback) {
     if (navigator) {
@@ -38,15 +64,13 @@ marker:google.maps.Marker;
 
   ngAfterViewInit() {
     this.getCurrentLocation(() => {
-      console.log(this.lat)
-      console.log(this.lng)
       this.mapInitializer();
     });
   }
 
   mapInitializer() {
     this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
-    // debugger
     this.marker.setMap(this.map);
+    this.showPosts();
   }
 }
