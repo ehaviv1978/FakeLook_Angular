365a0ee8277d0cc8bc4d29cc3e85d3f796982b21
ehaviv1978 <55824563+ehaviv1978@users.noreply.github.com>
2020-02-21 19:50:09 +0200

    change profile picture improved

--- src/app/change-user-picture/change-user-picture.component.ts
+++ src/app/change-user-picture/change-user-picture.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
 import { faPhone, faUserMd, faLock,faCamera  } from '@fortawesome/free-solid-svg-icons';
 import { HttpClient } from '@angular/common/http';
 
@@ -7,19 +7,21 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './change-user-picture.component.html',
   styleUrls: ['./change-user-picture.component.css']
 })
+
+
 export class ChangeUserPictureComponent implements OnInit {
   faCamera= faCamera;
   selectedFile :File = null;
   public imagePath;
   imgURL: any;
   public message: string;
-  
   constructor(private http: HttpClient,) { }
 
+
   onUpload(){
-    const fd = new FormData();
-    fd.append('image', this.selectedFile, this.selectedFile.name);
-    console.log(this.selectedFile);
+    // const fd = new FormData();
+    // fd.append('image', this.selectedFile, this.selectedFile.name);
+    console.log(this.imgURL);
     this.http.post('http://localhost:8888/api/users/picture', {"file": this.imgURL ,"userId": 2}).subscribe(res => {
       console.log(res)
     });
@@ -32,19 +34,41 @@ export class ChangeUserPictureComponent implements OnInit {
     var mimeType = files[0].type;
     if (mimeType.match(/image\/*/) == null) {
       this.message = "Only images are supported.";
+      document.getElementById("btnUpdate").style.visibility = "hidden";
+      document.getElementById("btnCancel").style.visibility = "hidden";
+      this.imgURL=null;
       return;
     }
- 
+    this.message =null;
+    document.getElementById("btnUpdate").style.visibility = "visible";
+    document.getElementById("btnCancel").style.visibility = "visible";
+
     var reader = new FileReader();
     this.imagePath = files;
-    console.log(this.imagePath[0].name)
+    //console.log(this.imagePath[0].name)
     reader.readAsDataURL(files[0]); 
     reader.onload = (_event) => { 
       this.imgURL = reader.result; 
     }
   }
 
+  
+  @ViewChild('file', {static: true}) fileUploader:ElementRef;
+  
+
+  onCancel(){
+    // document.getElementById("btnUpdate").style.visibility = "hidden";
+    //   document.getElementById("btnCancel").style.visibility = "hidden";
+    //   this.imagePath = null;
+    //   this.imgURL=null;
+    //   this.message =null;
+      this.fileUploader.nativeElement.value ="";
+      console.log(this.fileUploader.nativeElement);
+  }
+
   ngOnInit() {
+    document.getElementById("btnUpdate").style.visibility = "hidden";
+    document.getElementById("btnCancel").style.visibility = "hidden";
   }
 
 }
