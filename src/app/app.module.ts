22c6f11dc55b2f50500c3a2ca370e0a68f77b9e8
ehaviv1978 <55824563+ehaviv1978@users.noreply.github.com>
2020-02-22 17:00:34 +0200

    unused components deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule }    from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { MapPostsComponent } from './map-posts/map-posts.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
@@ -12,7 +11,6 @@ import { FormsModule } from '@angular/forms';
 import { AllPostsComponent } from './all-posts/all-posts.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { UserLoginComponent } from './user-login/user-login.component';
 import { NewPostComponent } from './new-post/new-post.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ChangeUserPictureComponent } from './change-user-picture/change-user-picture.component';
@@ -20,14 +18,12 @@ import { ChangeUserPictureComponent } from './change-user-picture/change-user-pi
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent,
     MapPostsComponent,
     NavBarComponent,
     SideBarComponent,
     AllUsersComponent,
     AllPostsComponent,
     CreateAccountComponent,
-    UserLoginComponent,
     NewPostComponent,
     ChangeUserPictureComponent,
   ],
