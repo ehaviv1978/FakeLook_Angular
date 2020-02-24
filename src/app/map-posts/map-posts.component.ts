790010cb1272992a29a743ac8d6c1ba305a48169
ehaviv1978 <ehaviv@hotmail.com>
2020-02-24 16:04:33 +0200

    marker clastering add

--- src/app/map-posts/map-posts.component.ts
+++ src/app/map-posts/map-posts.component.ts
@@ -1,9 +1,10 @@
-import { Component, AfterViewInit, ViewChild, ElementRef,Input } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, Input, EventEmitter, Output } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { DomSanitizer,SafeUrl } from '@angular/platform-browser';
-import {Post} from'../models/post';
-import {PostService} from '../services/post.service';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { Post } from '../models/post';
+import { PostService } from '../services/post.service';
 import { User } from '../models/user';
+import  MarkerClusterer from "@google/markerclusterer"
 
 
 @Component({
@@ -13,45 +14,57 @@ import { User } from '../models/user';
 })
 export class MapPostsComponent implements AfterViewInit {
   @Input() public parentData;
-  user:User;
+  user: User;
   title = 'angular-gmap';
-  @ViewChild('mapContainer',{static: true}) gmap: ElementRef;
+  @ViewChild('mapContainer', { static: true }) gmap: ElementRef;
   map: google.maps.Map;
-  mapOptions:google.maps.MapOptions;
-  marker:google.maps.Marker;
+  mapOptions: google.maps.MapOptions;
+  marker: google.maps.Marker;
   posts: Post[];
   post: Post;
-  imageurl:SafeUrl;
+  imageurl: SafeUrl;
   constructor(private postServ: PostService, private sanitizer: DomSanitizer) { }
+
+  @Output() clickPostEvent = new EventEmitter<Post>();
 
   markers = [];
   async showPosts() {
-    this.postServ.getPosts().subscribe( res => {
-      this.posts =  res;
+    this.postServ.getPosts().subscribe(res => {
+      this.posts = res;
       for (let post of this.posts) {
-        let marker =  new google.maps.Marker({
+        let marker = new google.maps.Marker({
           position: new google.maps.LatLng(post.lat, post.long),
           map: this.map,
-          title: post.description,
           icon: {
             url: post.userPic,
-            scaledSize: new google.maps.Size(40,40)
+            scaledSize: new google.maps.Size(40, 40)
           }
-         });
-         //let pic = post.picture
-         let  imageurl:SafeUrl = post.picture;
-
+        });
         let infowindow = new google.maps.InfoWindow({
-          content:'<div><img width="200" src="' + post.picture + '"/><br>' +post.description + '</div>'
+          content: '<div><img width="200" src="' + post.picture + '"/><br>' + post.description + '</div>'
         });
 
-        marker.addListener('click', () =>{
+        marker.addListener('mouseover', () => {
           infowindow.open(this.map, marker);
         });
+        marker.addListener('mouseout', () => {
+          infowindow.close();
+        });
+        marker.addListener('click', () => {
+          this.clickPostEvent.emit(post);
+        });
 
-         marker.setMap(this.map);
+        this.markers.push(marker);
+
+        marker.setMap(this.map);
       }
+
+      var options = {
+        imagePath: 'http://localhost:4200/assets/images/m'
+      };
+      new MarkerClusterer(this.map, this.markers, options)
     });
+    
   }
 
   getCurrentLocation(callback) {
@@ -68,7 +81,7 @@ export class MapPostsComponent implements AfterViewInit {
           title: 'I\'m Here!',
           icon: {
             url: this.user.picture,
-            scaledSize: new google.maps.Size(40,40)
+            scaledSize: new google.maps.Size(40, 40)
           }
         });
         callback();
@@ -88,7 +101,7 @@ export class MapPostsComponent implements AfterViewInit {
     this.showPosts();
   }
 
-  ngOnInit(){
-    this.user= this.parentData;
+  ngOnInit() {
+    this.user = this.parentData;
   }
 }
